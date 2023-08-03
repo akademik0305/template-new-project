eea6ffff45ee4cb865f105eab5f8c9c5e0d32709
akademik0305 <tuychiyevakademik@gmail.com>
2023-08-03 13:26:20 +0500

    upload template

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <NuxtPage></NuxtPage>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>hello world</h1>
+    <button class="button">123</button>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<!-- <style lang="scss" scoped> -->
+
+
+<!-- </style> -->
